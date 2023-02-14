30e326e2fae5d58fb43d2735ceb17feffe136471
Naveen E C <naveen.e1@exxonmobil.com>
2023-02-14 20:18:09 +0530

    login added

--- ConsoleApp1/NewClass.cs
+++ ConsoleApp1/NewClass.cs
@@ -7,12 +7,10 @@ using IntakeBase = Xom.Gci.Addin.LvMake.Intake.Base;
 using IIntakeBase = Xom.Gci.Addin.LvMake.IIntake;
 namespace ConsoleApp1
 {
-   public  class NewClass: IIntakeBase.IIntakeLIMSDataFetch  
+   public  class NewClass 
     {
 
-        LIMSDataModel FetchAndLoadMasterData(LvHelper lvHelper, string absoluteURL, string requestItemID);
-        void FetchAndLoadSheetData(LvHelper LvHelper, LIMSDataModel limsModel, IIntakeValidate validate, IIntakeContext context, IIntakeIngredient ingredient, IIntakeFormulation formulation, IIntakeBlends blends, IIntakeProcessVariables processVariables, IIntakeActualIng actualIng, IIntakeBatchContainer batchContainer, IIntakeInventory inventory, IIntakeRibbon ribbon, ConditionalFormattingHelper cfHelper, ColumnWidthHelper widthHelper, IIntakeTreatedIngredient treatedIngredient, IIntakeTreatedBatches treatedBatches, IIntakeTests tests, IIntakeContainerProcessVariables containerProcessVariables, IIntakeReviewComplete reviewComplete, bool isSimpleIntake = true);
-        void FetchAndLoadColorData(LIMSDataModel limsDataModel);
+     
 
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,28 +3,39 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using System.Net;
+
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Net.Http.Headers;
-using Newtonsoft.Json;
+
 using System.Net;
 using IntakeBase=Xom.Gci.Addin.LvMake.Intake.Base;
 using IIntakeBase = Xom.Gci.Addin.LvMake.IIntake;
+using Xom.Gci.Addin.LvMake.SimpleIntake;
+using Xom.Gci.Addin.LvMake.Helpers;
 
 namespace ConsoleApp1
 {
-    public class Program  :IntakeBase.IntakeLIMSDataFetch(null)
+    public class Program 
     {
 
         public static Excel.Workbook workbook ;
+        public static string userName = "naveene";
+        public static string password = "exxonmobilinfosys";
 
-        
-           public  void Main(string[] args)
+        public static string lvurl = "https://hoeapp910.na.xom.com/labvantage/rest";
+        public static string absoluteurl = "https://hoeapp910.na.xom.com/labvantage";
+        public static string databaseid = "LabVantageNADEV";
+        public LvHelper LvHelper { get; set; }
+        public  static void Main(string[] args)
         {
             //NewExcelFile();
+   
             IntakeFormExcelFile();
-            Login();
+           // Login();
         }
 
         public static void NewExcelFile()
@@ -79,8 +90,8 @@ namespace ConsoleApp1
 
         public static void IntakeFormExcelFile()
         {
-            string fileName = "R2301-009984-001_Intake.xlsx";
-            string sourcePath = @"C:\Users\naveene\Downloads";
+            string fileName = "R2302-010006-001_Intake.xlsx";
+            string sourcePath = @"C:\Users\naveene\Downloads\New folder (2)";
             string targetPath = @"C:\Users\naveene\Downloads\New folder";
 
             string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
@@ -112,10 +123,56 @@ namespace ConsoleApp1
                         Console.WriteLine("Loading the Excel File ");
                       
                         workbook = excelApp.Workbooks.Open(destFile);
-                        IntakeBase.IntakeValidate validate = new IntakeBase.IntakeValidate();
-                        IntakeBase.OpenWorkbook openWorkbook = new IntakeBase.OpenWorkbook();
-                        openWorkbook.LIMSDataFetch.FetchAndLoadSheetData();
-                                    worksheet = workbook.Worksheets[2];
+                            
+        //IntakeBase.IntakeValidate validate = new IntakeBase.IntakeValidate();
+        IntakeBase.OpenWorkbook openWorkbook = new IntakeBase.OpenWorkbook();
+                      
+                        var configWorksheet = (Excel.Worksheet)workbook.Sheets[GlobalValue.ConfigSheet];
+
+                        openWorkbook.ConfigHelper = new ConfigHelper(configWorksheet);
+
+
+                        openWorkbook.ConfigHelper.LoadConfigValues();
+
+                        Excel.Worksheet mainSheet = (Excel.Worksheet)workbook.Worksheets[GlobalValue.MainSheet];
+                   
+                        openWorkbook.AddinHelper = new SimpleIntakeAddinHelper(workbook, false);
+                        openWorkbook.Ribbon = new SimpleIntakeRibbon(workbook);
+                        openWorkbook.Inventory = new SimpleIntakeInventory(workbook);
+                        openWorkbook.ActualIngredient = new SimpleIntakeActualIng(workbook);
+                        openWorkbook.Formulation = new SimpleIntakeFormulation(workbook);
+                        openWorkbook.Tests = new SimpleIntakeTests(workbook);
+                        openWorkbook.LIMSDataFetch = new SimpleIntakeLIMSDataFetch(workbook);
+                        openWorkbook.DynamicConfigHelper = new DynamicConfigHelper(workbook);
+                        openWorkbook.DropdownHelper = new DropdownHelper(workbook);
+                        openWorkbook.SyncHelper = new SyncHelper(workbook);
+                        openWorkbook.ColumnWidthHelper = new ColumnWidthHelper(workbook);
+                        openWorkbook.LockCellsHelper = new LockCellsHelper(workbook);
+                        openWorkbook.CopyCellsHelper = new CopyCellsHelper(workbook);
+                        openWorkbook.CfHelper = new ConditionalFormattingHelper(workbook);
+                        openWorkbook.RibbonConfigHelper = new RibbonConfigHelper(workbook);
+                        openWorkbook.PrintConfigHelper = new PrintConfigHelper(workbook);
+                        openWorkbook.ChangeLogHelper = new ChangeLogHelper(workbook);
+                        openWorkbook.Ingredient = new SimpleIntakeIngredient(workbook, false);
+                        openWorkbook.TreatedIngredient = new SimpleIntakeTreatedIngredient(workbook, false);
+                        openWorkbook.TreatedBatches = new SimpleIntakeTreatedBatches(workbook);
+                        string userName = "naveene";
+                        string password = "exxonmobilinfosys";
+                        string lvurl = "https://hoeapp910.na.xom.com/labvantage/rest";
+                        string absoluteurl = "https://hoeapp910.na.xom.com/labvantage";
+                        string databaseid = "LabVantageNADEV";
+                        openWorkbook.LvHelper = new LvHelper(userName, password, lvurl, databaseid);
+
+                        openWorkbook.LvHelper.Login();
+                        openWorkbook.ProcessVariables = new SimpleIntakeProcessVariables(workbook);
+                        openWorkbook.VariableSetting = new SimpleIntakeVariableSetting(workbook);
+                        openWorkbook.Blends = new SimpleIntakeBlends(workbook, false);
+                        openWorkbook.BatchContainer = new SimpleIntakeBatchContainer(workbook);
+                        openWorkbook.Context = new SimpleIntakeContext(workbook, false);
+                        openWorkbook.ReviewComplete = new SimpleIntakeReviewComplete(workbook, false);
+                        openWorkbook.Validation = new SimpleIntakeValidation(workbook, false);
+                        var limsDataModel = openWorkbook.LIMSDataFetch.FetchAndLoadMasterData(openWorkbook.LvHelper, "https://hoeapp910.na.xom.com/labvantage", "R2302-010006-001");
+                        worksheet = workbook.Worksheets[2];
                         worksheet.Cells[5, 6].Value2 = "BTEC_10035";
                         worksheet.Cells[4, 6].Value2 = "BTEC";
                 
@@ -132,6 +189,35 @@ namespace ConsoleApp1
             }
         }
 
+        //private static HttpClient httpClient;
+        //private static HttpClientHandler httpHandler;
+        //private static string lvBaseUrl;
+        //private static string lvDatabaseId;
+        ////public static LvConnection connection;
+        //private static string lvConnectionID;
+        //private static string jSessionID;
+        //public static LvConnection connection = new LvConnection();
+        //public static bool IsUnauthorized { get; set; }
+        //public static void LvHelper(string username, string password, string baseUrl, string databaseId)
+        //{
+        //    // To adhere to security guidelines - LIMS portal upgraded to TSL level 1.2 & 1.3 
+        //    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+        //    httpHandler = new HttpClientHandler();
+        //    httpClient = new HttpClient(httpHandler);
+        //    lvBaseUrl = baseUrl;
+        //    lvDatabaseId = databaseId;
+        //    IsUnauthorized = true;
+        //    httpClient.BaseAddress = new Uri(lvBaseUrl);
+        //    lvConnectionID = string.Empty;
+        //    jSessionID = string.Empty;
+
+
+        //    connection.DatabaseId = lvDatabaseId;
+        //    connection.Username = username;
+        //    connection.Password = password;
+
+        //}
         public static void Login()
         {
             // To adhere to security guidelines - LIMS portal upgraded to TSL level 1.2 & 1.3 
